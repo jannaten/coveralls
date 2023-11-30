9a45885f21a3b49b2bc5d68d28afd4b14c011ece
Jannaten Nayem <jannaten.nayem@tuni.fi>
2023-11-30 15:11:46 +0200

    new changes

---
+++ tests/math.test.js
@@ -0,0 +1,9 @@
+const add = require('../src/math');
+
+test('adds 1 + 2 to equal 3', () => {
+  expect(add(1, 2)).toBe(3);
+});
+
+test('adds 5 + 7 to equal 12', () => {
+  expect(add(5, 7)).toBe(12);
+});
